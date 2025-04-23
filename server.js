f9d857b4faa3442222cdd907d2699d733a083195
Jorge Gómez <jorge.agomezdev@gmail.com>
2025-04-22 21:54:20 -0600

    agregando script

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
 // Conectar a MongoDB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
